2733245e57b99066ab283170bf1eeb6a229ae5b2
renan.horta <renan.horta96@gmail.com>
2024-12-02 20:29:26 -0300

    [add] Adicionada rotas entre o site. agora todos os componentes estão interligados via Link

--- chat_app/src/views/Chat/Chat.jsx
+++ chat_app/src/views/Chat/Chat.jsx
@@ -1,7 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Chat(){
     return(
-        <div>ESSA PAGINA É o Chat</div>
+        <div>
+            <Link to={"/usuarios"}>Voltar para lista de usuários</Link>
+            <ul>
+                <li> CHAT</li>
+                <li>TODAS AS MSGS</li>
+            </ul>
+
+            <form>
+                <label>Digite sua mensagem</label>
+                <input></input>
+            </form>
+        </div>
     )
 }
--- chat_app/src/views/UserForm/UserForm.jsx
+++ chat_app/src/views/UserForm/UserForm.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function UserForm(){
     return(
@@ -14,6 +15,7 @@ export default function UserForm(){
             <input type="text" />
             <button onClick={console.log("enviado")}>Cadastrar</button>
         </form>
+        <Link to={"/usuarios"}>Lista de usuários</Link>
         </>
     )
 }
