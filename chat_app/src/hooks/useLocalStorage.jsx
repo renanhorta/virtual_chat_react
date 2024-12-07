65e7605e3a5a871669574a77e3d98164a8d0da2d
renan horta <renan.horta96@gmail.com>
2024-12-07 16:23:08 -0300

    [update] CustomHook atualizado para trabalhar com listas

--- chat_app/src/hooks/useLocalStorage.jsx
+++ chat_app/src/hooks/useLocalStorage.jsx
@@ -1,25 +1,42 @@
 export const useLocalStorage = (key) => {
   const setItem = (value) => {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    //takes the existing list in localStorage (if there is one). If there is nothing, it starts with an empty list.
+    //The user is then added to this list, and the list is saved back to localStorage.
+
+    try {
+      const storedData = JSON.parse(window.localStorage.getItem(key)) || [];
+      storedData.push(value);
+      window.localStorage.setItem(key, JSON.stringify(storedData));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  return { setItem };
-};
+  const getItem = () => {
+    //Returns the entire list of users that are stored in localStorage, or an empty list if there is no data or if an error occurs.
 
-const setLocalItem = () => {
-  useEffect(() => {
-    localStorage.setItem("list-profile", JSON.stringify());
-  }, []);
-};
+    try {
+      const item = window.localStorage.getItem(key);
+      if (item) {
+        return JSON.parse(item);
+      }
+      return [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
 
-const getLocalItem = () => {
-  useEffect(() => {
-    localStorage.setItem("list-profile", JSON.stringify());
-  }, []);
-};
+  const removeItem = (value) => {
+    //receives an object (chosen by id), finds that user in the list and removes it. The updated list is then saved back to localStorage.
+    try {
+      const storedData = JSON.parse(window.localStorage.getItem(key)) || [];
+      const updatedData = storedData.filter((user) => user.id !== value.id);
+      window.localStorage.setItem(key, JSON.stringify(updatedData));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const editLocalItem = () => {
-  useEffect(() => {
-    localStorage.setItem("list-profile", JSON.stringify());
-  }, []);
+  return { setItem, getItem, removeItem };
 };
