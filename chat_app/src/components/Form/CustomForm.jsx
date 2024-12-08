6954e4a84e9cd2a0f19174fd0f9f6340807cc055
renan horta <renan.horta96@gmail.com>
2024-12-07 23:47:37 -0300

    [update] Atualizado o componente customform

--- chat_app/src/components/Form/CustomForm.jsx
+++ chat_app/src/components/Form/CustomForm.jsx
@@ -11,7 +11,7 @@ function Submit({ isPending }) {
 }
 
 export default function CustomUserForm() {
-  /** component responsible for registering new profiles in the application's localstorage.*/
+  /** component responsible for registering new profiles in the application's localstorage. */
   const [isPending, setIsPending] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -19,6 +19,9 @@ export default function CustomUserForm() {
   const [photoUrl, setPhotoUrl] = useState("");
   const [errors, setErrors] = useState({});
   const [successMessage, setSuccessMessage] = useState(""); // Show a message when the profile is saved
+
+  // use the custom hook to manage profiles in localStorage
+  const { setItem } = useLocalStorage("Profiles");
 
   const validateName = (name) => {
     // This regex checks that the name contains only letters (including accented characters, like é/ç/ã and others) and spaces.
@@ -35,13 +38,13 @@ export default function CustomUserForm() {
     // Regex to check if the email is in a valid format, containing a user, an “@” and a domain.
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return "Insira um email válido. ex: ";
+      return "Insira um email válido.";
     }
     return null;
   };
 
   const validateAge = (age) => {
-    // the age must be graeter than 0
+    // The age must be greater than 0
     if (age <= 0) {
       return "A idade deve ser maior que zero.";
     }
@@ -50,26 +53,22 @@ export default function CustomUserForm() {
 
   const handleSubmit = async (event) => {
     /**
-     * This function controls the submission  of the form.
+     * This function controls the submission of the form.
      * First, the function prevents the standard submit event from working, after which it makes a switch in the “isPending”
      * state to simulate sending information to a back-end and takes all the values from the form inputs and validates each one
      * independently.
-     * If they are all validated, the form saves the form values in a “profile” object in localStorage in the “listProfile” field.
-     * */
+     * If they are all validated, the form saves the form values in a “profile” object in localStorage in the “Profiles” field.
+     */
     event.preventDefault();
     setIsPending(true);
 
-    // use the custom hook to set a new item in the localStorage
-    const { setItem } = useLocalStorage("Profiles");
-
-    // check the inputs fields and valitade the values.
+    // Validate inputs fields and capture error messages
     const nameError = validateName(name);
     const emailError = validateEmail(email);
     const ageError = validateAge(Number(age));
 
     if (nameError || emailError || ageError) {
-      // checks if the error constants are empty (null), if not, an object will be created in setErrors that contains the phrase of each
-      // error for each field. And to execute the handleSubmit function.
+      // If any error exists, update the errors object and stop the submission process
       setErrors({
         name: nameError,
         email: emailError,
@@ -78,10 +77,11 @@ export default function CustomUserForm() {
       setIsPending(false);
       return;
     }
-    // if all the errors is Null the setErros will be a empty object to follow the submition.
+
+    // If no errors, clear the errors object
     setErrors({});
 
-    // create a new profile, with the forms inputs and a empty msg array, to be saved in the localStorage
+    // Create a new profile object to be saved in the localStorage
     const newProfile = {
       id: new Date().getTime(),
       name: name,
@@ -91,22 +91,22 @@ export default function CustomUserForm() {
       messages: [],
     };
 
-    setItem(newProfile);
-    // simulate a back-end comunication
+    setItem(newProfile); // Save the new profile in the localStorag
+
+    // Simulate a back-end communication delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
     setIsPending(false);
 
-    // show success message
+    // Show success message
     setSuccessMessage("Perfil cadastrado!");
     setTimeout(() => setSuccessMessage(""), 2000);
 
-    // clear the form fields
+    // Clear the form fields
     setName("");
     setEmail("");
     setAge(0);
     setPhotoUrl("");
   };
-
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <label htmlFor="name" className={styles.label}>
